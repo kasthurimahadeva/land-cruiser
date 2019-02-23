1ede84049ea636cb45c301974c14c7af49f03817
kasthurimahadeva <kasthuri.14@cse.mrt.ac.lk>
2019-02-23 15:49:36 +0530

    angular-8

--- Angular/src/app/components/spinner/spinner.component.ts
+++ Angular/src/app/components/spinner/spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpinnerComponent implements OnInit {
 
+  @Input() isLoading: boolean;
   constructor() { }
 
   ngOnInit() {
