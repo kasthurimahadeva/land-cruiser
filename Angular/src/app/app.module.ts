3f4b2d9da2775ce79e610d354394f782dad52f3f
Gayashan <gayashan.k@eyepax.com>
2019-02-23 14:09:38 +0530

    angular-9

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from "./app.component";
 import { RantListComponent } from './components/rant-list/rant-list.component';
 import { RantDetailsComponent } from './components/rant-details/rant-details.component';
 
+
+const appRoutes: Routes = [
+  {path:'rant-list', 
+  component:RantListComponent,
+ },
+ {
+   path: '',
+   component: RantListComponent,
+   pathMatch: 'full'
+ },
+ {
+   path: 'rant-details',
+   component: RantDetailsComponent
+ }
+];
+
+
+
+
 @NgModule({
   declarations: [AppComponent, RantListComponent, RantDetailsComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,
+            RouterModule.forRoot(appRoutes)
+           ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
+
 export class AppModule {}
